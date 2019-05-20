013cb245718ded44fb348a78d761f84f331318cd
Salome Desauty <salome.desauty@epita.fr>
2019-05-20 21:34:09 +0200

    Cmake, src and test directories

---
+++ src/main.cc
@@ -0,0 +1,11 @@
+#include "chessboard-interface.hh"
+#include "listener.hh"
+#include "pgn-move.hh"
+#include "pgn-parser.hh"
+#include "position.hh"
+#include "report-type.hh"
+
+int main()
+{
+    return 0;
+}
