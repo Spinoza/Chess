7b35a42ec4610a850dde1e33fca5d65222339d99
Salome Desauty <salome.desauty@epita.fr>
2019-05-21 11:45:41 +0200

    [MOVE] starting fct structure

---
+++ src/move.cc
@@ -0,0 +1,41 @@
+#include "move.hh"
+
+std::bitset<64> Chessboard::diag
+{
+
+}
+
+std::bitset<64> Chessboard::diag1
+{
+
+}
+
+std::bitset<64> Chessboard::straight
+{
+
+}
+
+std::bitset<64> Chessboard::straight
+{
+
+}
+
+std::bitset<64> Chessboard::straight
+{
+
+}
+
+std::bitset<64> Chessboard::L()
+{
+
+}
+
+std::bitset<64> Chessboard::generateMove(PieceType P, int pos) const
+{
+    std::bitset<64> result;
+}
+
+int numberMoves(const std::bitset<64>& table) const
+{
+    return table::count();
+}
---
+++ src/move.hh
@@ -0,0 +1,16 @@
+#pragma once
+#include "chessboard.hh"
+
+using namespace board;
+
+enum class Move
+{
+    DIAG,
+    DIAG1,
+
+    STRAIGHT,
+    STRAIGHT1,
+    STRAIGHT2,
+
+    L,
+}
