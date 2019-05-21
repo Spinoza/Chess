909af28bacb49381a0eca8027a781b9b47ac06f3
Salome Desauty <salome.desauty@epita.fr>
2019-05-21 17:28:03 +0200

    [move] diag1 and straight1 started

--- src/move.cc
+++ src/move.cc
@@ -1,36 +1,90 @@
 #include "move.hh"
+#define A_C std::bitset<64>(0x7f7f7f7f7f7f7f7f)
+#define H_C std::bitset<64>(0xfefefefefefefefe)
+#define 1_L std::bitset<64>(0xffffffffffffff00)
+#define 8_L std::bitset<64>(0xffffffffffffff)
 
-std::bitset<64> Chessboard::diag()
+
+std::bitset<64> Chessboard::diag(std::bitset<64> position)
 {
 
 }
 
-std::bitset<64> Chessboard::diag1()
+std::bitset<64> Chessboard::diag1(std::bitset<64> position)
+{
+    //right up diag
+    std::bitset<64> t1(position);
+    t1 &= H_C; // AND assign
+    t1 &= 8_L;
+    t1 += 9;
+    //right down diag
+    std::bitset<64> t2(position);
+    t2 &= H_C;
+    t2 &= 1_L;
+
+    //left up diag
+    std::bitset<64> t3(position);
+    t3 &= A_C;
+    t3 &= 8_L;
+    t3 += 7;
+    //left down diag
+    std::bitset<64> t4(position);
+    t4 &= A_C;
+    t4 &= 1_L;
+
+    t1 |= t3; //OR assign ---> concatenation of the 2 tabs
+    t2 |= t4;
+    return t1 |= t2;
+}
+
+std::bitset<64> Chessboard::straight(std::bitset<64> position)
 {
 
 }
 
-std::bitset<64> Chessboard::straight()
+std::bitset<64> Chessboard::straight1(std::bitset<64> position)
+{
+    //up
+    std::bitset<64> t1(position);
+    t1 &= 8_L;
+    t1 -= 8; //shift top
+    //down
+    std::bitset<64> t2(position);
+    t2 &= 1_L;
+    t2 += 8;//shift down
+
+    //right
+    std::bitset<64> t1(position);
+    t3 &= H_C;
+    t3 ++;
+
+    //left
+    std::bitset<64> t2(position);
+    t4 &= A_C;
+    t4 --;
+
+    t1 |= t3;
+    t1 |= t4;
+    return t1 |= t2;
+}
+
+
+std::bitset<64> Chessboard::forward1(std::bitset<64> position, bool color)
 {
 
 }
 
-std::bitset<64> Chessboard::straight1()
+std::bitset<64> Chessboard::forward2(std::bitset<64> position, bool color)
 {
 
 }
 
-std::bitset<64> Chessboard::straight2()
+std::bitset<64> Chessboard::L(std::bitset<64> position)
 {
 
 }
 
-std::bitset<64> Chessboard::L()
-{
-
-}
-
-std::bitset<64> Chessboard::generateMove(PieceType P, Chessboard position) const
+std::bitset<64> Chessboard::generateMove(PieceType P, std::bitset<64> position) const
 {
     switch(P)
     {
