6f2c05409af62b76dfe63a47e65bd4eef51564af
Victor de Coninck <victor.de-coninck@epita.fr>
2019-05-22 03:40:47 +0200

    [BISHOP] bishop moves

--- src/bishop.cc
+++ src/bishop.cc
@@ -1,21 +1,135 @@
+#include <iostream>
 #include "chessboard.hh"
 
-std::bitset<64> Chessboard::moveBishopNE(std::bitset<64> test, int dir) const
+std::bitset<64> Chessboard::moveBishopNE(std::bitset<64> position) const
 {
-    return test;
+    std::bitset<64> borders = std::bitset<64>(0x7f7f7f7f7f7f7f7f);
+    std::bitset<64> otherPiece = full_board();
+
+    std::bitset<64> trace (0x00);
+    std::bitset<64> otherTrace (0x00);
+
+    std::bitset<64> currentPos = position << 7;
+    std::bitset<64> valid = currentPos & borders;
+
+    while(valid != 0)
+    {
+        trace |= valid;
+        currentPos = valid << 7;
+        valid = currentPos & borders;
+    }
+
+    otherPiece = trace & otherPiece;
+    currentPos = otherPiece << 7;
+
+    valid = currentPos & borders;
+
+    while(valid != 0)
+    {
+        otherTrace |= valid;
+        currentPos = valid << 7;
+        valid = currentPos & borders;
+    }
+
+    return trace ^ otherTrace;
+
 }
 
-std::bitset<64> Chessboard::moveBishopNW(std::bitset<64> test, int dir) const
+std::bitset<64> Chessboard::moveBishopSE(std::bitset<64> position) const
 {
-    return test;
+    std::bitset<64> borders = std::bitset<64>(0x7f7f7f7f7f7f7f7f);
+    std::bitset<64> otherPiece = full_board();
+
+    std::bitset<64> trace (0x00);
+    std::bitset<64> otherTrace (0x00);
+
+    std::bitset<64> currentPos = position >> 9;
+    std::bitset<64> valid = currentPos & borders;
+
+    while(valid != 0)
+    {
+        trace |= valid;
+        currentPos = valid >> 9;
+        valid = currentPos & borders;
+    }
+
+    otherPiece = trace & otherPiece;
+    currentPos = otherPiece >> 9;
+
+    valid = currentPos & borders;
+
+    while(valid != 0)
+    {
+        otherTrace |= valid;
+        currentPos = valid >> 9;
+        valid = currentPos & borders;
+    }
+
+    return trace ^ otherTrace;
 }
 
-std::bitset<64> Chessboard::moveBishopSE(std::bitset<64> test, int dir) const
+std::bitset<64> Chessboard::moveBishopSW(std::bitset<64> position) const
 {
-    return test;
+    std::bitset<64> borders = std::bitset<64>(0xfefefefefefefefe);
+    std::bitset<64> otherPiece = full_board();
+
+    std::bitset<64> trace (0x00);
+    std::bitset<64> otherTrace (0x00);
+
+    std::bitset<64> currentPos = position >> 7;
+    std::bitset<64> valid = currentPos & borders;
+
+    while(valid != 0)
+    {
+        trace |= valid;
+        currentPos = valid >> 7;
+        valid = currentPos & borders;
+    }
+
+    otherPiece = trace & otherPiece;
+    currentPos = otherPiece >> 7;
+
+    valid = currentPos & borders;
+
+    while(valid != 0)
+    {
+        otherTrace |= valid;
+        currentPos = valid >> 7;
+        valid = currentPos & borders;
+    }
+
+    return trace ^ otherTrace;
 }
 
-std::bitset<64> Chessboard::moveBishopSW(std::bitset<64> test, int dir) const
+std::bitset<64> Chessboard::moveBishopNW(std::bitset<64> position) const
 {
-    return test;
+    std::bitset<64> borders = std::bitset<64>(0xfefefefefefefefe);
+    std::bitset<64> otherPiece = full_board();
+
+    std::bitset<64> trace (0x00);
+    std::bitset<64> otherTrace (0x00);
+
+    std::bitset<64> currentPos = position << 9;
+    std::bitset<64> valid = currentPos & borders;
+
+    while(valid != 0)
+    {
+        trace |= valid;
+        currentPos = valid << 9;
+        valid = currentPos & borders;
+    }
+
+    otherPiece = trace & otherPiece;
+    currentPos = otherPiece << 9;
+
+    valid = currentPos & borders;
+
+    while(valid != 0)
+    {
+        otherTrace |= valid;
+        currentPos = valid << 9;
+        valid = currentPos & borders;
+    }
+
+    return trace ^ otherTrace;
 }
